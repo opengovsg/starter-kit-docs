07e09f15704d9f89dd85cb27c23191c17eed16c6
Ian Chen <ian-@gmx.com>
2023-06-19 15:03:11 +0800

    chore: config for custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,10 +24,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://opengovsg.github.io",
+  url: "https://start.open.gov.sg",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/starter-kit-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
